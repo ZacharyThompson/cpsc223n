d6804d035e3922fc91c393e2c41716178394936f
Zachary Thompson <zachary@Zacharys-MacBook-Pro.local>
2021-09-11 15:32:03 -0700

    Add beginning of assignment 2

---
+++ assign2/StopLight.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Windows.Forms;
+public class StopLight
+{
+	static void Main(string[] args)
+	{
+		StopLightUI StopLightApp = new StopLightUI();
+		Application.Run(StopLightApp);
+	}
+}
+
---
+++ assign2/StopLightUI.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Timers;
+
+public class StopLightUI : Form
+{
+	public StopLightUI()
+	{
+
+	}
+}
