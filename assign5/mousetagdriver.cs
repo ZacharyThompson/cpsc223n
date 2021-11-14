7fbcfef0e1bbb0696d8954f53dbb4130d02cc42e
Zachary Thompson <zachthompson406@gmail.com>
2021-11-14 13:21:02 -0800

    Initial class definitions and Makefile

---
+++ assign5/mousetagdriver.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Windows.Forms;
+public class MouseTag
+{
+	static void Main(string[] args)
+	{
+		MouseTagUI MouseTagApp = new MouseTagUI();
+		Application.Run(MouseTagApp);
+	}
+}
---
+++ assign5/mousetagui.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Timers;
+
+public class MouseTagUI : Form
+{
+	public MouseTagUI()
+	{
+	}
+}
